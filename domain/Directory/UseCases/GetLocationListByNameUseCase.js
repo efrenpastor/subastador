6c9219b829b97295921f8fc9a5f69f802d0683b6
Efren Pastor <efrenpastor@192.168.1.9>
2022-11-17 17:26:17 +0100

    feature: cache get locations by name use case

--- domain/Directory/UseCases/GetLocationListByNameUseCase.js
+++ domain/Directory/UseCases/GetLocationListByNameUseCase.js
@@ -2,28 +2,40 @@ import { DomainError, UseCase } from "../../domain";
 import { GenericError } from "../Errors/Location/GenericError";
 import { LocationNameValueObject } from "../Model/Location/LocationNameValueObject";
 import { HTTPLocationRepository } from "../Repositories/Location/HTTPLocationRepository";
+import { Cache } from '../../../utils/Cache'
 
 export class GetLocationListByNameUseCase extends UseCase {
     _repository
     _config
+    _cache
 
     static create({ config }) {
         const repository = HTTPLocationRepository.create({ config })
-        return new GetLocationListByNameUseCase({ repository, config })
+        const cache = Cache.create({ limit: 10 })
+        return new GetLocationListByNameUseCase({ repository, cache, config })
     }
 
-    constructor({ repository, config }) {
+    constructor({ repository, cache, config }) {
         super()
         this._repository = repository
         this._config = config
+        this._cache = cache
     }
 
     async execute({ locationName }) {
         try {
+            const cacheKey = `GetLocationListByNameUseCase#execute:${locationName}`
+            const cached = this._cache.get(cacheKey)
+            if (cached) {
+                return cached.toJSON()
+            }
+
             const locationNameValueObject = LocationNameValueObject.create({ locationName })
             const locationListEntity = await this._repository.getLocationListByName({
                 locationName: locationNameValueObject
             })
+            this._cache.set(cacheKey, locationListEntity)
+
             return locationListEntity.toJSON()
         } catch (err) {
             return Promise.reject(
